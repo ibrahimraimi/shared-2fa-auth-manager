85b8ac49b3e4d446b5da562011db6bf55ca9b611
Ibrahim Raimi <raimiibrahim44@gmail.com>
2025-02-26 12:34:50 +0100

    fix: fixed CORS issues by updating allowed origins in backend

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -2,8 +2,11 @@ import { getEnv } from "../common/utils/get-env";
 
 const appConfig = () => ({
   NODE_ENV: getEnv("NODE_ENV", "development"),
-  APP_ORIGIN: getEnv("APP_ORIGIN", "localhost"),
-  PORT: getEnv("PORT", "5000"),
+  APP_ORIGIN: getEnv(
+    "APP_ORIGIN",
+    "https://shared-2fa-auth-manager.onrender.com"
+  ),
+  PORT: getEnv("PORT", "3000"),
   BASE_PATH: getEnv("BASE_PATH", "/api/v1"),
   MONGO_URI: getEnv("MONGO_URI"),
   JWT: {
